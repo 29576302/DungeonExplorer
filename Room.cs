5555610936fb5fd47e1d5de42bb7d59c2cc74201
Borisz Kantor <29576302@students.lincoln.ac.uk>
2025-03-13 09:45:10 +0000

    Added XML comments.

--- Room.cs
+++ Room.cs
@@ -2,12 +2,22 @@
 
 namespace DungeonExplorer
 {
+    /// <summary>
+    /// This class is used to create rooms that the player explores.
+    /// </summary>
     public class Room
     {
         public Monster Monster { get; private set; }
         public List<Potion> Potions { get; private set; }
         public Weapon Weapon { get; private set; }
         private string Description; // Accessed through methods detailed below.
+        /// <summary>
+        /// This is the constructor for the Room class.
+        /// </summary>
+        /// <param name="monster" >The monster that will occupy the room.</param>
+        /// <param name="potions">The potions that will be in the room.
+        /// A list of potions is used, as more than one can appear in a room.</param>
+        /// <param name="weapon">The weapon that will be in the room.</param>
         public Room(Monster monster, List<Potion> potions, Weapon weapon)
         {
             Monster = monster;
@@ -15,7 +25,9 @@ namespace DungeonExplorer
             Weapon = weapon;
             Description = "";
         }
-        // Generates a description of the room.
+        /// <summary>
+        /// Generates a description of the room.
+        /// </summary>
         public void CreateDescription()
         {
             string description = "Room Contents:";
@@ -55,7 +67,10 @@ namespace DungeonExplorer
             }
             Description = description;
         }
-        // Returns the description of the room after making sure that it is up to date with the current contents.
+        /// <summary>
+        /// Returns the description of the room after making sure that it is up to date with the current contents.
+        /// </summary>
+        /// <returns></returns>
         public string GetDescription()
         {
             CreateDescription();
